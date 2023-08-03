e24b1311fbf266ad1dcaaeb17762066a26a043ca
ryanjnam99 <ryanjnam@yahoo.com>
2023-08-02 23:38:45 -0700

    ed

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ for (var i=1; i<101; i++) {
     if (i%15 == 0) {
         console.log("Fizzbuzz")
     }
-    else if(i%3 == 0) {
-        console.log("Fizz");
+    else if (i%5 == 0) {
+        console.log("buzz");
     }
-    else if(i%5 == 0) {
-        console.log("Buzz")
+    else if (i%3 == 0) {
+        console.log("fizz");
     }
     else {
         console.log(i);
